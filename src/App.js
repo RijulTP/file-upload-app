5c8105ee7708a89d0846dfa99ba0abac4ef6e90d
RijulTP <rijulrajtkeey2@gmail.com>
2024-03-18 20:35:17 +0530

    Modified appjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+// import axios from "axios";
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null)
+
+  const onFileChange = (event) => {
+    setSelectedFile(event.target.files[0])
+  }
+
+  const onFileUpload = () => {
+    console.log("File Uploaded")
+  }
+
+  const fileData = () => {
+    if (selectedFile) {
+      return (
+        <div>
+          <h2>File Details:</h2>
+          <p>File Name: {selectedFile.name}</p>
+          <p>File Type: {selectedFile.type}</p>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+        </div>
+      )
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>File Upload using React!</h1>
+        <div>
+          <input type="file" onChange={onFileChange} />
+          <button onClick={onFileUpload}>Upload!</button>
+        </div>
+        {fileData()}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
